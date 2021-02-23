4aba820f7a520e92d265cead11a5c20446756284
szatolokin <staszatolokin28@gmail.com>
2021-02-23 21:11:01 +0300

    Revert project

--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,4 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: ['csv-loader'],
-            },
-            {
-                test: /\.xml$/,
-                use: ['xml-loader'],
-            },
-        ],
-    },
 };
