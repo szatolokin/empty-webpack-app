8238de1a087f4090fcf8ff0bf1096a5ffbe9fdb9
szatolokin <staszatolokin28@gmail.com>
2021-02-23 20:52:09 +0300

    Add fonts loader & example

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,11 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
                 type: 'asset/resource',
             },
         ],
