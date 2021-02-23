c6b5b8fe7cd3bd22c98015f1cf13eacc572fd695
szatolokin <staszatolokin28@gmail.com>
2021-02-23 21:00:57 +0300

    Add data, install csv,xml-loader & example

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(csv|tsv)$/,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader'],
+            },
         ],
     },
 };
